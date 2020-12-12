cf74aae24e2167753a1937fb8c89d6ec671360dd
Gabriel Matei <gabrielvmatei@gmail.com>
2020-12-12 16:26:57 +0200

    fixed post seeder

--- seeders/20201212132306-demo-post.js
+++ seeders/20201212132306-demo-post.js
@@ -6,15 +6,18 @@ const { random } = require('faker');
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const usersQuery = await models.User.findAll();
-    const usersPosts = new Array(100).map(post => ({
-      userId: usersQuery[Math.random() % usersQuery.length].id,
-      media: faker.internet.url(),
-      title: faker.lorem.sentence(),
-      body: faker.lorem.text(),
-      upvotes: faker.random.number(9999),
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    }));
+    const usersPosts = new Array(100).fill(0).map(post => {
+      const user = usersQuery[Math.floor(Math.random() * usersQuery.length)]
+      return {
+        userId: user.id,
+        media: faker.image.imageUrl(),
+        title: faker.lorem.sentence(),
+        body: faker.lorem.paragraph(),
+        upvotes: faker.random.number(9999),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    });
     await queryInterface.bulkInsert('Posts', usersPosts, {});
   },
 

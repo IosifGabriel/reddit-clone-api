7835c35f01b7d2eb134219515d3adf5a6edc1c9f
GabrielIosif <gabriel.iosif06@gmail.com>
2021-01-04 22:20:13 +0200

    added login and authentication

--- seeders/20201212125924-demo-user.js
+++ seeders/20201212125924-demo-user.js
@@ -1,12 +1,16 @@
 'use strict';
 const faker = require('faker');
+const config = require('../config/appConfig');
+const bcrypt = require('bcrypt');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const hashedPassword = await bcrypt.hash('AdminReddit', config.SALT_ROUNDS);
+
     const mockUsers = new Array(10).fill().map(() => ({
         username: faker.internet.userName(),
         email: faker.internet.email(),
-        password: faker.internet.password(),
+        password: hashedPassword,
         createdAt: new Date(),
         updatedAt: new Date(),
     }));

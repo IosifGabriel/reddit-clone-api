0bf00cb7fc1a84aa6dc3f733fcf913000f6ff4f8
Gabriel Matei <gabrielvmatei@gmail.com>
2021-01-11 23:55:31 +0200

    filter threads and posts

--- graphql/queries/post.js
+++ graphql/queries/post.js
@@ -1,13 +1,25 @@
-const { GraphQLNonNull, GraphQLInt, GraphQLList } = require('graphql')
+const { GraphQLNonNull, GraphQLInt, GraphQLString, GraphQLList } = require('graphql')
 const models = require('../../models')
+const { Op } = require('sequelize')
 const postType = require('../types/postType')
 
 module.exports = {
   posts: {
     type: GraphQLList(postType),
     description: 'Get a list of all posts',
-    resolve: async (_) => {
-      const posts = await models.Post.findAll()
+    args: {
+      title: {
+        type: GraphQLString
+      },
+    },
+    resolve: async (_, { title = '' }) => {
+      const posts = await models.Post.findAll({
+        where: {
+          title: {
+            [Op.like]: `%${title}%`
+          }
+        }
+      })
       return posts
     }
   },
--- graphql/queries/thread.js
+++ graphql/queries/thread.js
@@ -1,13 +1,25 @@
-const { GraphQLList } = require('graphql')
+const { GraphQLList, GraphQLString } = require('graphql')
 const models = require('../../models')
+const { Op } = require('sequelize')
 const threadType = require('../types/threadType')
 
 module.exports = {
   threads: {
     type: GraphQLList(threadType),
     description: 'Get a list of all threads',
-    resolve: async () => {
-      const threads = await models.Thread.findAll()
+    args: {
+      name: {
+        type: GraphQLString
+      },
+    },
+    resolve: async (_, { name = '' }) => {
+      const threads = await models.Thread.findAll({
+        where: {
+          name: {
+            [Op.like]: `%${name}%`
+          }
+        }
+      })
       return threads
     }
   }
